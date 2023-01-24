e3147f39dcf4b885394f2f2e867aed802d767fa4
LisaJacklin <lmjacklin@alaska.edu>
2023-01-23 19:26:52 -0900

    Lab0prep

---
+++ Lab0_VHDL/DCD3x8.vhd
@@ -0,0 +1,21 @@
+----------------------------------------------------------------------------------
+--Lisa Jacklin
+-- EE 443 Lab 0
+-- 1/24/2022
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+
+entity DCD3x8 is
+--  Port ( );
+end DCD3x8;
+
+architecture DCD3x8_behavioral of DCD3x8 is
+begin
+
+
+end DCD3x8_behavioral;
